685369c3cb086dc6ac3c0e87d8a50ce35acac160
Victor <victorribera92@gmail.com>
2022-12-14 16:49:33 +0100

    cards

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,19 +3,23 @@ import { Stack, Box } from "@mui/material";
 
 import { ChannelCard, Loader, VideoCard } from "./";
 
-const Videos = ({videos}) => {
-  console.log(videos)
-return (
-    <Stack direction={"column" || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
-      {/* {videos.map((item, idx) => (
+const Videos = ({ videos }) => {
+  return (
+    <Stack
+      direction={"row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
         <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
-      ))} */}
-      <Box>Videos</Box>
+      ))}
     </Stack>
   );
-}
+};
 
 export default Videos;
